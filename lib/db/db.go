aaa806deb309e2758f40a015b3becffdaae4e330
nts <nterheoid@gmail.com>
2022-11-24 01:57:06 +0900

    implementing cache

--- lib/db/db.go
+++ lib/db/db.go
@@ -1,9 +1,29 @@
 package db
 
+import (
+	"errors"
+	"fmt"
+	"reflect"
+)
 
-//always look into cache first, go DB if no cache is found.
-func Get() {
+// always look into cache first, go DB if no cache is found.
+func Get(model interface{}) error {
 
+	t := reflect.TypeOf(model)
+	if t.Kind() != reflect.Ptr {
+		return errors.New("model passed in must be a pointer")
+	}
+
+	v := reflect.ValueOf(model).Elem()
+
+	pkey := reflect.ValueOf(v.Field(0).Interface())
+	table := t.Elem().Name()
+
+	cacheKey := fmt.Sprintf("%s|%v", table, pkey)
+	println(cacheKey)
+	//looking up cache
+
+	return nil
 }
 
 func CachedInsert() {
@@ -14,6 +34,6 @@ func CachedUpdate() {
 
 }
 
-func DeleteWithCache(){
+func DeleteWithCache() {
 
 }
