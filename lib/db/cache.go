cf7c440ba9376a46b43d00f8c379b9246cc9f243
nts <nterheoid@gmail.com>
2022-11-25 17:44:03 +0900

    cache

--- lib/db/cache.go
+++ lib/db/cache.go
@@ -2,39 +2,92 @@ package db
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
+	"reflect"
 
 	"github.com/go-redis/redis/v9"
 )
 
-func WriteCacheOnly() {
+func saveCache(model interface{}) error {
+	t := reflect.TypeOf(model)
+	if t.Kind() != reflect.Ptr {
+		return errors.New("passed in model must be a pointer")
+	}
 
-	var ctx = context.Background()
-	rdb := client()
-	err := rdb.Set(ctx, "key", "value", 0).Err()
+	rc := redis.NewClient(&redis.Options{
+		Addr: ":6379",
+	})
+
+	key := t.Elem().Name()
+	ctx := context.Background()
+	//modelVal := reflect.ValueOf(model).Elem()
+	field := getPkeyValue(model)
+
+	val, err := json.Marshal(model)
 	if err != nil {
-		panic(err)
+		return nil
+	}
+	err = rc.HSet(ctx, key, field, val).Err()
+	if err != nil {
+		return err
 	}
 
+	return nil
+	/*
+		key := makeCacheKeyName(model)
+		if _, err := rc.Pipelined(ctx, func(p redis.Pipeliner) error {
+			for i := 0; i < modelVal.NumField(); i++ {
+				field := reflect.ValueOf(modelVal.Field(i).Interface())
+				//fmt.Println(modelVal.Type().Field(i).Type == time.Time)
+				p.HSet(ctx, key, modelVal.Type().Field(i).Name, fmt.Sprintf("%v", field))
+			}
+			return nil
+		}); err != nil {
+			return err
+		}
+		return nil
+	*/
 }
 
-func ReadCacheOnly() {
-
-	var ctx = context.Background()
-	rdb := client()
-
-	val2, err := rdb.Get(ctx, "key2").Result()
-	if err == redis.Nil {
-		fmt.Println("key2 does not exist")
-	} else if err != nil {
-		panic(err)
-	} else {
-		fmt.Println("key2", val2)
+func getCache(destModel interface{}) error {
+	t := reflect.TypeOf(destModel)
+	if t.Kind() != reflect.Ptr {
+		return errors.New("passed in model must be a pointer")
 	}
 
+	rc := redis.NewClient(&redis.Options{
+		Addr: ":6379",
+	})
+
+	key := t.Elem().Name()
+	ctx := context.Background()
+	field := getPkeyValue(destModel)
+
+	val, err := rc.HGet(ctx, key, field).Bytes()
+	if err != nil {
+		return err
+	}
+
+	json.Unmarshal(val, destModel)
+	return nil
 }
 
-func client() *redis.Client {
+// the first field of passed in model is supposed to be the primary key.
+func getPkeyValue(model interface{}) string {
+	t := reflect.TypeOf(model)
+	if t.Kind() != reflect.Ptr {
+		panic("passed in model must be a pointer")
+	}
+
+	v := reflect.ValueOf(model).Elem()
+	pkeyVal := reflect.ValueOf(v.Field(0).Interface())
+
+	return fmt.Sprintf("%v", pkeyVal)
+}
+
+func cacheClient() *redis.Client {
 	rdb := redis.NewClient(&redis.Options{
 		Addr:     "localhost:6379",
 		Password: "", // no password set
@@ -42,3 +95,23 @@ func client() *redis.Client {
 	})
 	return rdb
 }
+
+/*
+// return "" on failure
+func getCacheKeyName(model interface{}) string {
+
+	t := reflect.TypeOf(model)
+	if t.Kind() != reflect.Ptr {
+		panic("passed in model must be a pointer")
+	}
+
+	v := reflect.ValueOf(model).Elem()
+
+	pkeyVal := reflect.ValueOf(v.Field(0).Interface())
+	table := t.Elem().Name()
+
+	cacheKey := fmt.Sprintf("%s|%v", table, pkeyVal)
+
+	return cacheKey
+}
+*/
--- lib/db/db.go
+++ lib/db/db.go
@@ -1,27 +1,12 @@
 package db
 
-import (
-	"errors"
-	"fmt"
-	"reflect"
-)
-
 // always look into cache first, go DB if no cache is found.
 func Get(model interface{}) error {
 
-	t := reflect.TypeOf(model)
-	if t.Kind() != reflect.Ptr {
-		return errors.New("model passed in must be a pointer")
+	err := getCache(model)
+	if err != nil {
+		return err
 	}
-
-	v := reflect.ValueOf(model).Elem()
-
-	pkey := reflect.ValueOf(v.Field(0).Interface())
-	table := t.Elem().Name()
-
-	cacheKey := fmt.Sprintf("%s|%v", table, pkey)
-	println(cacheKey)
-	//looking up cache
 
 	return nil
 }
