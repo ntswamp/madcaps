b84581a036b8d16c8757e0cdb9b064922bf9f0a3
nts <nterheoid@gmail.com>
2022-11-25 19:42:01 +0900

    db

--- lib/db/db.go
+++ lib/db/db.go
@@ -1,9 +1,15 @@
 package db
 
+import "github.com/go-redis/redis/v9"
+
 // always look into cache first, go DB if no cache is found.
 func Get(model interface{}) error {
 
 	err := getCache(model)
+	//no cache for this query, 1)fetch from db. 2)cache the record.
+	if err == redis.Nil {
+
+	}
 	if err != nil {
 		return err
 	}
@@ -20,5 +26,9 @@ func CachedUpdate() {
 }
 
 func DeleteWithCache() {
+
+}
+
+func dbClient() {
 
 }
